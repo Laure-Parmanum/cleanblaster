8bb6d6d3a1278cb7c43b9d3ddc0da14d79a3b888
Arnaud Maillard <arnmai09@gmail.com>
2023-08-23 10:10:00 -0700

    first

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,24 @@
 class PagesController < ApplicationController
   def home
   end
+
+  class CategoriesController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def description
+    end
+
+    def booking
+    end
+
+    def approval
+    end
+
+    def notification
+    end
+  end
 end
---
+++ db/migrate/20230823152020_create_bookings.rb
@@ -0,0 +1,11 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :cleaner, null: false, foreign_key: true
+      t.boolean :confirmed
+
+      t.timestamps
+    end
+  end
+end
