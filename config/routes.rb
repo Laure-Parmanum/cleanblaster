3a1892d3e73daa8a0036e1160f7e7d23b44b7213
Arnaud Maillard <arnmai09@gmail.com>
2023-08-24 09:23:53 -0700

    pull

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
   # resources :categories, only: [:index, :show] do
   #   member do
   #     get 'description'
