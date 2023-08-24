6183a7249fc752f48cbda2cfca40b7d3f9938140
Arnaud Maillard <arnmai09@gmail.com>
2023-08-24 10:18:16 -0700

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'categories/index'
+  get 'categories/show'
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/categories/:id', to: 'categories#show', as: 'custom_show'
+
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
+
   # resources :categories, only: [:index, :show] do
   #   member do
   #     get 'description'
